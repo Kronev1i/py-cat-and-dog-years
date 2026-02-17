f4bfc5c39570c04e4b4e9db0a1d362f02e9f3b26
unknown <kgoncharenko83@gmail.com>
2026-02-17 20:51:12 +0100

    COMPLETE

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,47 @@
-from app.main import get_human_age
+import pytest
+import app.main as main
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17]),
+    ],
+    ids=[
+        "0 cat/dog years -> 0 human age",
+        "14 cat/dog years -> 0 human age",
+        "15 cat/dog years -> 1 human age",
+        "23 cat/dog years -> 1 human age",
+        "24 cat/dog years -> 2 human age",
+        "27 cat/dog years -> 2 human age",
+        "28 cat/dog years -> 3/2 human age",
+        "100 cat/dog years -> 21/17 human age",
+    ]
+)
+def test_get_human_age(
+        cat_age: int,
+        dog_age: int,
+        expected: int
+) -> None:
+    result = main.get_human_age(
+        cat_age,
+        dog_age
+    )
+    assert isinstance(
+        result,
+        list
+    ), "Result should be a list"
+    assert result == expected, \
+        (
+            f"For cat_age={cat_age}, "
+            f"dog_age={dog_age}, "
+            f"expected {expected}, "
+            f"got {result}"
+        )
